29e6c8f358ae014afc8dc6daad380cff01603809
johndoe26 <john_doe@163.com>
2015-12-26 17:33:18 +0800

    update the import items

--- src/main/java/org/broadleafcommerce/vendor/alipay/web/controller/AliPayPartnerController.java
+++ src/main/java/org/broadleafcommerce/vendor/alipay/web/controller/AliPayPartnerController.java
@@ -14,7 +14,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
